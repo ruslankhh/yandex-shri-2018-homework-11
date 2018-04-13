57a80a11dc1c0dbe3e3c2b2d242a53e810b64092
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-13 19:10:30 +0300

    test: Обновил тесты

--- test/Request/RequestWithXHR.test.js
+++ test/Request/RequestWithXHR.test.js
@@ -9,25 +9,15 @@ const requestURL = 'http://localhost:5000';
 const postsURL = url.resolve(requestURL, 'posts');
 const commentsURL = url.resolve(requestURL, 'comments');
 const profileURL = url.resolve(requestURL, 'profile');
-const badURL = url.resolve(requestURL, 'abracadabra');
+const errorURL = url.resolve(requestURL, 'abracadabra');
+const badURL = 'http://abracadabra';
 
 describe('RequestWithXHR', () => {
-  const expected1 = data.posts;
-  const expected2 = data.comments;
-  const expected3 = data.profile;
-  const expectedError = {};
-
-  const getExpect = (res, expected, expectedStatus = 200) => {
-    const result = JSON.parse(res.responseText);
-
-    expect(res.status).to.equal(expectedStatus);
-    expect(result).to.deep.equal(expected);
-  };
-
   it('обработка одного запроса', next => {
     const request = new RequestWithXHR();
-    const handler = function (req, resArr) {
-      getExpect(resArr[0], expected1);
+    const handler = function (res, err) {
+      expect(res[0].status).to.equal(200);
+      expect(res[0].data).to.deep.equal(data.posts);
     };
 
     request
@@ -35,10 +25,23 @@ describe('RequestWithXHR', () => {
       .then(() => next());
   });
 
+  it('обработка запроса с ошибкой', next => {
+    const request = new RequestWithXHR();
+    const handler = function (res, err) {
+      expect(res[0].status).to.equal(404);
+      expect(res[0].data).to.deep.equal({});
+    };
+
+    request
+      .get(errorURL, handler, handler)
+      .then(() => next());
+  });
+
   it('обработка неверного запроса', next => {
     const request = new RequestWithXHR();
-    const handler = function (req, resArr) {
-      getExpect(resArr[0], expectedError, 404);
+    const handler = function (res, err) {
+      expect(res[0].status).to.equal(0);
+      expect(res[0].data).to.deep.equal({});
     };
 
     request
@@ -48,48 +51,66 @@ describe('RequestWithXHR', () => {
 
   it('обработка нескольких последовательных запросов', next => {
     const request = new RequestWithXHR();
-    const handler1 = function (req, resArr) {
-      getExpect(resArr[0], expected1);
+    const handler = function (res, err) {
+      expect(res[0].status).to.equal(200);
+      expect(res[0].data).to.deep.equal(data.posts);
     };
-    const handler2 = function (req, resArr) {
-      getExpect(resArr[0], expected2);
+    const handler1 = function (res, err) {
+      expect(res[1].status).to.equal(200);
+      expect(res[1].data).to.deep.equal(data.comments);
     };
-    const handler3 = function (req, resArr) {
-      getExpect(resArr[0], expected3);
+    const handler2 = function (res, err) {
+      expect(res[2].status).to.equal(200);
+      expect(res[2].data).to.deep.equal(data.profile);
     };
 
     request
-      .get(postsURL, handler1, handler1)
-      .get(commentsURL, handler2, handler2)
-      .get(profileURL, handler3, handler3)
+      .get(postsURL, handler, handler)
+      .get(commentsURL, handler1, handler1)
+      .get(profileURL, handler2, handler2)
       .then(() => next());
   });
 
   it('доступ к ответам на предыдущие запросы', next => {
     const request = new RequestWithXHR();
-    const handler1 = function (req, resArr) {
-      getExpect(resArr[0], expected1);
+    const handler = function (res, err) {
+      expect(res[0].data).to.deep.equal(data.posts);
     };
-    const handler2 = function (req, resArr) {
-      getExpect(resArr[0], expected2);
-      getExpect(resArr[1], expected1);
+    const handler1 = function (res, err) {
+      expect(res[0].data).to.deep.equal(data.posts);
+      expect(res[1].data).to.deep.equal(data.comments);
     };
-    const handler3 = function (req, resArr) {
-      getExpect(resArr[0], expected3);
-      getExpect(resArr[1], expected2);
-      getExpect(resArr[2], expected1);
+    const handler2 = function (res, err) {
+      expect(res[0].data).to.deep.equal(data.posts);
+      expect(res[1].data).to.deep.equal(data.comments);
+      expect(res[2].data).to.deep.equal(data.profile);
     };
-    const handlerLast = function (resArr) {
-      getExpect(resArr[0], expected3);
-      getExpect(resArr[1], expected2);
-      getExpect(resArr[2], expected1);
+    const handler3 = function (res, err) {
+      expect(res[0].data).to.deep.equal(data.posts);
+      expect(res[1].data).to.deep.equal(data.comments);
+      expect(res[2].data).to.deep.equal(data.profile);
+      expect(res[3].status).to.deep.equal(0);
+      expect(res[3].data).to.deep.equal({});
+      expect(err[3].code).to.equal('ENOTFOUND');
+    };
+    const handlerResponses = function (res) {
+      expect(res[0].data).to.deep.equal(data.posts);
+      expect(res[1].data).to.deep.equal(data.comments);
+      expect(res[2].data).to.deep.equal(data.profile);
+      expect(res[3].status).to.deep.equal(0);
+      expect(res[3].data).to.deep.equal({});
+    };
+    const handlerErrors = function (err) {
+      expect(err[3].code).to.equal('ENOTFOUND');
     };
 
     request
-      .get(postsURL, handler1, handler1)
-      .get(commentsURL, handler2, handler2)
-      .get(profileURL, handler3, handler3)
-      .then(handlerLast)
+      .get(postsURL, handler, handler)
+      .get(commentsURL, handler1, handler1)
+      .get(profileURL, handler2, handler2)
+      .get(badURL, handler3, handler3)
+      .then(handlerResponses)
+      .catch(handlerErrors)
       .then(() => next());
   });
 });
